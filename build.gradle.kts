c570d9c581549ff22fdb0c82a901894525556fe1
Artemiy Smirnov <artemiy.smirnov@htlstp.at>
2024-09-07 13:36:21 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.3.3"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "dev.eagely"
+version = "0.0.1"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
---
+++ src/main/kotlin/dev/eagely/voice/Application.kt
@@ -0,0 +1,11 @@
+package dev.eagely.voice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
